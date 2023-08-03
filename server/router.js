77113f069eb47352a4f9608b6fcc899ea2dcc47f
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-08-03 14:14:30 +1200

    Remodified code for router

--- server/router.js
+++ server/router.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const repository = require("./repository");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const getGrades = await repository.getGradeScale();
+    
+    res.json(getGrades);
 
   } catch (err) {
     next(err);
